4ff71fef93643a76d19d80a45a717e27263eebd2
Ihor Peresunko <marazmatic77@gmail.com>
2019-01-13 20:36:59 +0200

    Load logic added

--- practice/benchmark.py
+++ practice/benchmark.py
@@ -1,8 +1,30 @@
 import time as t
+from functools import wraps
 
 
 def print_duration(function, *args):
     start = t.time()
     function(*args)
     end = t.time()
-    print('Function [', function.__name__, '] took', '%.4f' % (end - start), 'milliseconds')
+    print("Function [", function.__name__, "] took", "%.4f" % (end - start), "milliseconds")
+
+
+def measure_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = t.time()
+        result = func(*args, **kwargs)
+        stop = t.time()
+        print(f"Function [{func.__name__}] took {stop - start:.4f} seconds")
+        return result
+
+    return wrapper
+
+
+if __name__ == "__main__":
+
+    @measure_time
+    def test_time(timeout):
+        t.sleep(timeout)
+
+    test_time(5)
--- practice/solution.py
+++ practice/solution.py
@@ -1,91 +1,78 @@
-from string import Template
+from pprint import pprint
+
+from benchmark import measure_time
 
 
-class Slice :
-    __x1 = 0
-    __y1 = 0
-    __x2 = 0
-    __y2 = 0
-    __template = Template("$y1 $x1 $y2 $x2")
-
+class Slice:
     def __init__(self, x1, y1, x2, y2):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
 
     def transpose(self):
         pass
 
     @property
     def cords(self):
-        return self.__template.substitute(self.__y1, self.__x1, self.__y2, self.__x2)
+        return f"{self.y1} {self.x1} {self.y2} {self.x2}"
 
 
-class Pizza :
-    __start_x = 0
-    __start_y = 0
-    __height  = 0
-    __width   = 0
-    __data    = list()
-    __slices  = list()
+class Pizza:
+    def __init__(self, dataset):
+        self.data = list()
+        self.slices = list()
 
-    def __init__(self, config):
-        self._height = config['rows']
-        self._width = config['cols']
+        self.height = 0
+        self.width = 0
+        self.min_ing = 0
+        self.max_cell = 0
 
-    def add_row(self, row):
-        self.__data.append(row)
+        self.current_x = 0
+        self.current_y = 0
+        self.load_dataset(dataset)
+
+    @measure_time
+    def load_dataset(self, dataset):
+        with open(dataset) as file:
+            raw_conf = file.readline()
+            self.height, self.width, self.min_ing, self.max_cell = [int(i) for i in raw_conf.split()]
+
+            for line in file:
+                line = line.strip()
+                self.data.append(list(line))
 
     def is_cut(self):
-        return self.__start_x + 1 == self._width and \
-               self.__start_y + 1 == self._height
+        return self.current_x + 1 == self.width and self.current_y + 1 == self.height
 
     def meets_conditions(self):
         # TODO
         pass
 
+    @measure_time
     def cut_slices(self):
         while not self.is_cut():
-            end_x, end_y = [0,0]
+            end_x, end_y = [0, 0]
 
             while not self.meets_conditions():
                 # TODO
                 pass
 
-            p_slice = Slice(self.__start_x, self.__start_y, end_x, end_y)
-            self.__slices.append(p_slice)
+            p_slice = Slice(self.current_x, self.current_y, end_x, end_y)
+            self.slices.append(p_slice)
 
+    @measure_time
     def save_result(self):
-        output = open("output.out", mode='w')
-        output.write(str(len(self.__slices)))
-        for _slice in self.__slices:
-            output.write(_slice.coords)
-
-
-def parse_config(line):
-    r, c, l, h = line.split(sep=' ')
-
-    return {
-        'rows': int(r),
-        'cols': int(c),
-        'min_ingredients': int(l),
-        'slice_limit': int(h),
-    }
-
-
-def parse_pizza(dataset_, pizza_):
-    for line in dataset_:
-        pizza_.add_row(list(filter(lambda char: not char == '\n', line)))
+        result = "\n".join([item.cords for item in self.slices])
+        with open("output.out", mode="w") as file:
+            file.write(f"{len(self.slices)}\n")
+            file.write(result)
 
 
 if __name__ == "__main__":
-    from benchmark import print_duration
-
-    dataset = open('datasets/d_big.in', encoding='UTF-8')
-    pizza = Pizza(config=parse_config(dataset.readline()))
-
-    print_duration(parse_pizza, dataset, pizza)
-    print_duration(pizza.cut_slices)
-    print_duration(pizza.save_result)
-
+    pizza = Pizza(dataset="datasets/b_small.in")
+    # Tests
+    pprint(pizza.data)
+    test = [Slice(i, i, i, i) for i in range(5)]
+    pizza.slices = test
+    pizza.save_result()
