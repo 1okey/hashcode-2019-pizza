b5d1d4818c63638700cdd8e208b4d82a12e31cdc
AntonSkochko <antonskochko@gmail.com>
2019-02-16 17:17:27 +0200

    added final solutio + cleaned up folder

--- benchmark.py
+++ benchmark.py
@@ -1,15 +1,7 @@
 import time as t
 from functools import wraps
 
-
-def print_duration(function, *args):
-    start = t.time()
-    function(*args)
-    end = t.time()
-    print("Function [", function.__name__, "] took", "%.4f" % (end - start), "milliseconds")
-
-
-def measure_time(func):
+def measure_time(func, msg=""):
     @wraps(func)
     def wrapper(*args, **kwargs):
         start = t.time()
--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 from itertools import product
 from collections import Counter
-
+from os import path, listdir
 from benchmark import measure_time
 
-
+root = path.abspath("./")
 class Slice:
     def __init__(self, x1, y1, x2, y2):
         self.x1 = x1
@@ -28,25 +28,25 @@ class Slice:
 class Pizza:
     ING_COUNT = 2
 
-    def __init__(self, dataset):
+    def __init__(self, f_name):
         self.data = list()
         self.slices = list()
 
         self.height = 0
         self.width = 0
-        self.min_ing = 0
+        self.min_ingridients = 0
         self.max_size = 0
-
-        self.current_x = 0
-        self.current_y = 0
-        self.load_dataset(dataset)
+        self.x1 = 0
+        self.y1 = 0
+        
+        self.load_dataset(f"{root}/datasets/{f_name}.in")
         self.combinations = self.create_combinations()
 
     @measure_time
     def load_dataset(self, dataset):
         with open(dataset) as file:
             raw_conf = file.readline()
-            self.height, self.width, self.min_ing, self.max_size = [int(i) for i in raw_conf.split()]
+            self.height, self.width, self.min_ingridients, self.max_size = [int(i) for i in raw_conf.split()]
 
             for line in file:
                 line = line.strip()
@@ -61,7 +61,7 @@ class Pizza:
         return sorted(data, key=lambda tup: tup[0] + tup[1])
 
     def is_cut(self):
-        return self.current_y >= self.height
+        return self.y1 >= self.height
 
     def meets_conditions(self, slice):
         if not slice:
@@ -73,48 +73,54 @@ class Pizza:
 
         ing_cond = len(ingridients.keys()) == self.ING_COUNT
         size_cond = sum(ingridients.values()) <= self.max_size
-        M_cond = ingridients.get('M', 0) >= self.min_ing
-        T_cond = ingridients.get('T', 0) >= self.min_ing
+        M_cond = ingridients.get('M', 0) >= self.min_ingridients
+        T_cond = ingridients.get('T', 0) >= self.min_ingridients
 
         return all([ing_cond, size_cond, M_cond, T_cond])
 
     @measure_time
     def cut_slices(self):
         while not self.is_cut():
-            is_sliced = False
+            if self.x1 < self.width and self.y1 < self.height and self.data[self.y1][self.x1] == '*':
+                self.y1 += self.x1 // self.width
+                self.x1 = self.x1 % self.width + 1
+                continue
+
             for combo_y, combo_x in self.combinations:
-                y2 = (self.current_y + combo_y)
-                x2 = (self.current_x + combo_x)
+                y2 = (self.y1 + combo_y)
+                x2 = (self.x1 + combo_x)
 
-                slice_col = slice(self.current_x, x2)
-                p_slice = [self.data[y][slice_col] for y in range(self.current_y, y2) if (y + 1) <= self.height]
+                slice_col = slice(self.x1, x2)
+                p_slice = [self.data[y][slice_col] for y in range(self.y1, y2) if (y + 1) <= self.height]
                 if self.meets_conditions(p_slice):
-                    self.slices.append(Slice(self.current_x, self.current_y, x2 - 1, y2 - 1))
+                    self.slices.append(Slice(self.x1, self.y1, x2 - 1, y2 - 1))
 
-                    for y in range(self.current_y, y2):
+                    for y in range(self.y1, y2):
                         if (y + 1) <= self.height:
-                            for x in range(self.current_x, x2):
+                            for x in range(self.x1, x2):
                                 self.data[y][x] = '*'
-
-                    self.current_x = x2 % self.width
-                    self.current_y = self.current_y if x2 < self.width else y2
-                    is_sliced = True
                     break
-            if not is_sliced:
-                self.current_x += 1
-                if self.current_x >= self.width:
-                    self.current_x %= self.width
-                    self.current_y += 1
+
+            self.y1 += self.x1 // self.width
+            self.x1 = self.x1 % self.width + 1
+
 
     @measure_time
     def save_result(self, f_name):
         result = "\n".join([item.cords for item in self.slices])
-        with open(f"{f_name}.out", mode="w") as file:
+        with open(f"{root}/results/{f_name}.out", mode="w") as file:
             file.write(f"{len(self.slices)}\n")
             file.write(result)
 
 
+def solve_problem(file_name):
+    print(f"================= Starting {file_name} dataset =================")
+    pizza = Pizza(f_name=file_name)
+    pizza.cut_slices()
+    pizza.save_result(f_name=file_name)
+    print(f"================= Finished cutting {file_name} ================\n")
+
 if __name__ == "__main__":
-    pizza = Pizza(dataset="./datasets/b_small.in")
-    # Tests
-    pizza.save_result(f_name='b_small')
+    for in_file in ["a_example", "b_small", "c_medium", "d_big"]:
+        solve_problem(in_file.split('.')[0])    
+
