7dc4aa002708352309930a202f96bf1fa1619b38
Ihor Peresunko <marazmatic77@gmail.com>
2019-02-28 23:43:45 +0200

    added filtering

--- qualification.py
+++ qualification.py
@@ -14,33 +14,25 @@ def read_input(file_name):
             orientation, _, *tags = line.split(' ')
             photos_index[p_index] = dict(tags=set(tags), orientation=orientation, index=p_index)
             for tag in tags:
-                tags_index[tag].append(dict(orientation=orientation, tags=set(tags), index=p_index, used=False))
+                tags_index[tag].append(p_index)
         return [number_slides, tags_index, photos_index]
 
 
-def find_interesting(photos_index, tags_index, cur_photo):
-    possible_photos = list()
+def find_interesting(photos_index, tags_index, current_photo):
+    possible_photos = set()
     best_photo = None
-    interesting = None
-    if cur_photo is None:
-        print('AAAAAAAAAAAAAAAAAAAAAAAaa')
+    interesting = 0
 
-    current_tag_photo = cur_photo['tags']
-    for tag in cur_photo['tags']:
-        possible_photos.extend(tags_index[tag])
+    current_tag_photo = current_photo['tags']
+    for tag in current_photo['tags']:
+        possible_photos.update(set(tags_index[tag]))
 
-    possible_photos = {photo['index']: photo for photo in possible_photos if not photo['used']}
-    possible_photos = possible_photos.values()
+    # possible_photos = [photo for photo in possible_photos if not photos_index[photo]['used']]
     for photo in possible_photos:
-        if photo['index'] == cur_photo['index']:
+        if photo == current_photo['index']:
             continue
 
-        if interesting is None:
-            best_photo = photo
-            interesting = 0
-            continue
-
-        possible_tags = photos_index[photo['index']]['tags']
+        possible_tags = photos_index[photo]['tags']
         inter = len(possible_tags.intersection(current_tag_photo))
         if interesting < inter:
             best_photo = photo
@@ -48,7 +40,8 @@ def find_interesting(photos_index, tags_index, cur_photo):
         if interesting >= len(current_tag_photo) / 2:
             break
 
-    return best_photo
+    if best_photo:
+        return photos_index[best_photo]
 
 
 @measure_time
@@ -59,23 +52,29 @@ def create_slides(photos_index, tags_index):
     photos_index[index]['used'] = True
     slides = list()
     while index <= len(photos_index.keys()):
-        if not last_photo:
-            break
+        # if not last_photo:
+        #     break
+        index += 1
         if last_photo['orientation'] == 'H':
             new_photo = find_interesting(photos_index, tags_index, last_photo)
+            if not new_photo:
+                continue
             slides.append(str(last_photo['index']))
             last_photo['used'] = True
             last_photo = new_photo
             continue
         if last_photo['orientation'] == 'V':
             first_interesting = find_interesting(photos_index, tags_index, last_photo)
+            if not first_interesting:
+                continue
             second_interesting = find_interesting(photos_index, tags_index, first_interesting)
+            if not second_interesting:
+                continue
             slides.append('{}, {}'.format(last_photo['index'], first_interesting['index']))
             first_interesting['used'] = True
             last_photo['used'] = True
             last_photo = second_interesting
             continue
-        index += 1
     return slides
 
 
@@ -90,10 +89,10 @@ def save_result(file_name, result):
 
 def solve_problem(file_name):
     number_slides, tags_index, photos_index = read_input(file_name)
-    pprint(photos_index)
-    pprint(tags_index)
-    # slides = create_slides(photos_index, tags_index)
-    # save_result(file_name, slides)
+    # pprint(photos_index[0])
+    # pprint(tags_index)
+    slides = create_slides(photos_index, tags_index)
+    save_result(file_name, slides)
 
 
 if __name__ == "__main__":
